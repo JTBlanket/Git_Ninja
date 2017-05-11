30df4a993c0409c5ded2d56fe6eeb390712616f7
JTBlanket <j.blankenship1717@gmail.com>
2017-05-11 12:14:05 -0600

    Bug fix 19 spellcheck

--- src/Ninjitsu.java
+++ src/Ninjitsu.java
@@ -4,7 +4,7 @@ public class Ninjitsu {
 	public static void main(String[] args) {
 		System.out.println("Initial Ninja");
 		System.out.println("Experiment with new, risky GUI.");
-		System.out.println("Bugfix19");
+		System.out.println("Bug fix 19");
 	}
 
 }
