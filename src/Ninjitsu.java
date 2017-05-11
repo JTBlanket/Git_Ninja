0a79d07b09250b944315e363b30e858ca2a43362
JTBlanket <j.blankenship1717@gmail.com>
2017-05-11 12:27:13 -0600

    Added Swing message

--- src/Ninjitsu.java
+++ src/Ninjitsu.java
@@ -4,6 +4,12 @@ public class Ninjitsu {
 	public static void main(String[] args) {
 		System.out.println("Initial Ninja");
 		System.out.println("Experiment with new, risky GUI.");
+		
+		
+		
+		
+		
+		System.out.println("New Swing GUI works Perfecly");
 	}
 
 }
